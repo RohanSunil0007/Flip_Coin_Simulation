c2501f11a5dd0bfdac65c91685941cb7b6a35280
Rohan Sunil <reach.rohan0007@gmail.com>
2020-09-30 23:39:58 +0530

    Added : Functionality to check heads or tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Flip Coin Simulation Problem "
+isHeads=1
+randomCheck=$((RANDOM%2))
 
+if [ $randomCheck -eq $isHeads ]
+then
+   echo "Heads is the winner"
+else
+   echo "Tails is the winner"
+fi
