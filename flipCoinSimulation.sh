b1e40480c8668efa86f1b93d085358cd7bb903ae
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-01 00:12:17 +0530

    Added : Functionality to display Number Of times Heads or tails won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulation Problem "
 isHeads=1
-randomCheck=$((RANDOM%2))
+head=0
+tail=0
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
+do
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq $isHeads ]
+	then
+   	echo "Heads is the winner"
+		((head++))
+	else
+   	echo "Tails is the winner"
+		((tail++))
+	fi
+done
 
-if [ $randomCheck -eq $isHeads ]
-then
-   echo "Heads is the winner"
-else
-   echo "Tails is the winner"
-fi
+echo "Head won $head times"
+echo "Tail won $tail times"
